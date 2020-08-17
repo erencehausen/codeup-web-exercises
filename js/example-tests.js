be6625c1d0b01b70e248fdf2f34c6bc159687157
erencehausen <eric.d.rencehausen@gmail.com>
2020-08-17 13:42:51 -0700

    added for_loops_tests.js

--- js/example-tests.js
+++
@@ -1,8 +0,0 @@
-describe("returnExampleMsg", function() {
-    it("should be defined", function() {
-        expect(returnExampleMsg).toBeDefined();
-    });
-    it("should return example message", function() {
-        expect(returnExampleMsg()).toEqual("This is an example");
-    });
-});
